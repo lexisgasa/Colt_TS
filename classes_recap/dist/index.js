5602d1fa5be7c223e20cbae3fefdb8442b8cb4a0
Alexis <lexisgasa3007@gmail.com>
2023-08-29 22:46:03 +0200

    classes

--- classes_recap/dist/index.js
+++ classes_recap/dist/index.js
@@ -1,26 +1,29 @@
+"use strict";
+
+
 class Player {
-    score = 0;
-    numLives = 5;
+    // ! podemos poner propiedades que tengan de default y que todas las intancias que se creen de esta clase las hereden
+    #score = 0; //! con el # se hace una propiedad privada
+    numLives = 10;
+    // ! el constructor es una funcion que se llamara en cuanto se cree la clase, se usa normalmente para obtener data en los argumentos
     constructor (name, lastName) {
-        this.name = name,
-        this.lastName = lastName
+        this.name = name;
+        this.lastName = lastName;
     }
-    taunt () {
-        console.log("BOOYAH!")
-    };
-    // taunt = () => { console.log("first") } //! dos maneras de declarar metodos con la palabra funcion, que puede ser omitida, o funciones de flecha
-    greet(){
-        console.log(`Hey there, this is ${this.name} ${this["lastName"]}`)
-    };
-    loseLife(){
-        this.numLives -= 1;
-    };
+    // ! los metodos son funciones sin mas, cada vez que se llame esa instancia en especifico restara del parametro, recuerda que los metodos se pueden tambien escribir con una funcion de flecha -----  loseLife = () => this.numLives -= 1;
+    loseLife() {
+        return this.numLives -= 1;
+    }
+    // ! utilizando propiedades privadas, en este caso el score
+    getScore () {
+        return this.#score;
+    }
+    // ! los getters son metodos pero que actuan como propiedades y no funciones per se, por tanto para llamarles no hace falta invocarlas () ---- player1.fullName  seria suficiente
+    get fullName() {
+        return `${this.name} ${this.lastName}`
+    }
+   
 }
 
-const player1 = new Player("Henry","Muller");
-player1["taunt"]() //! notacion en bracket
-player1["greet"]()
-const player2 = new Player();
-player1.taunt() //! notacion de bracket o de punto
-
+const player1 = new Player("Alex", "Garcia");
 
