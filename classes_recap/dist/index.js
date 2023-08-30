a7c26540a19213b438f609c7c0a228196c2a5139
Alexis <lexisgasa3007@gmail.com>
2023-08-31 00:23:22 +0200

    ts classes

--- classes_recap/dist/index.js
+++ classes_recap/dist/index.js
@@ -1,1 +1,8 @@
 "use strict";
+class Player {
+    constructor(first, last) {
+        this.first = first,
+            this.last = last;
+    }
+}
+const elton = new Player("Elton", "Steele");
---
+++ classes_recap/src/index.ts
@@ -0,0 +1,10 @@
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string, last: string) {
+        this.first = first,
+        this.last = last;
+    }
+}
+
+const elton = new Player("Elton", "Steele");
