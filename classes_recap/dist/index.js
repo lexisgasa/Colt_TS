8959cdf9adfbbfb4ad22de98e6e34c703c6bf2c2
Alexis <lexisgasa3007@gmail.com>
2023-08-31 22:54:24 +0200

    ts classes

--- classes_recap/dist/index.js
+++ classes_recap/dist/index.js
@@ -1,8 +1,14 @@
 "use strict";
 class Player {
     constructor(first, last) {
-        this.first = first,
-            this.last = last;
+        // ! si :number no se usa, typescript inferira el tipo igualmente
+        this.score = 0;
+        this.name = first,
+            this.lastName = last;
     }
 }
 const elton = new Player("Elton", "Steele");
+/*
+elton.name = "Andrew" //! complain if changed since its a readonly property
+elton.lastName = "Iron"  //! lastname will be iron
+*/
--- classes_recap/src/index.ts
+++ classes_recap/src/index.ts
@@ -1,10 +1,20 @@
 class Player {
-    first: string;
-    last: string;
-    constructor(first: string, last: string) {
-        this.first = first,
-        this.last = last;
+    // ! con las propiedades readonly, una vez se cree una instancia esas propiedades no van a ser modificables
+    // ! los parametros que se van a usar en el constructor se tienen que crear primero o dara error
+    // ! todo lo que este en public se podra acceder desde afuera
+    public readonly name: string;
+    private readonly lastName: string;
+    // ! si :number no se usa, typescript inferira el tipo igualmente
+    public score: number = 0;
+
+    constructor(first: string, last: string) { //! anotacion de tipo en cuanto a los argumentos que recibira 
+        this.name = first,
+        this.lastName = last;
     }
 }
 
 const elton = new Player("Elton", "Steele");
+/*
+elton.name = "Andrew" //! complain if changed since its a readonly property
+elton.lastName = "Iron"  //! lastname will be iron
+*/
