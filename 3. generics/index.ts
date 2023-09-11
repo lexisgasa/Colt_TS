5f0903d19c7fdde9f5d5a0b9f55a1bf2fa181a32
Alexis <lexisgasa3007@gmail.com>
2023-09-11 23:42:26 +0200

    generics

---
+++ 3. generics/index.ts
@@ -0,0 +1,3 @@
+// un array se puede declarar o bien del tipo que sera el array como en la linea 2, o bien mediante un generico en el cual se especifica que es un array y entre <> el tipo de dato que contiene
+// const nums: number[] = [];
+const nums: Array<number> = []
